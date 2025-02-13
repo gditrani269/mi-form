a8e7e7796dcd4d8c9df4cf4cbc8afdaa846dbe59
unknown <gditrani269@gmail.com>
2025-02-13 16:37:26 -0300

    boto nhacer algo

--- src/components/MiRow2.jsx
+++ src/components/MiRow2.jsx
@@ -1,7 +1,19 @@
 import { useContext } from "react"
 import { NavLink } from "react-router-dom"
 
+import { MiTabla } from './MiTabla.jsx'
+
+import React, { useState } from "react";
+
 export const MiRow2 = ({id, accion, valor, Cantidad, Saldo_pesos, Saldo_dolares}) => {
+
+    const [Flag, setFlag] = useState (0);
+    async function handleButtonClick2() {
+        console.log ("MiRow2 - handleButtonClick2");
+        if (Flag == 0) {setFlag (1)}
+            else {setFlag (0)};
+        const cPaso = "anduvo"
+    }
 
     return (
 
@@ -13,8 +25,18 @@ export const MiRow2 = ({id, accion, valor, Cantidad, Saldo_pesos, Saldo_dolares}
                     <p className="card-text">valor: { valor }</p>
                     <p className="card-text">Pesos: { Saldo_pesos }</p>
                     <p className="card-text">Dolares: { Saldo_dolares }</p>
-                    <button className="btn btn-primary"
+                    <button className="btn btn-primary" onClick={handleButtonClick2}
                         >hacer algo</button>
+                    {Flag == 1 && (
+                            <tr><td>
+                                <div className="card">
+                                    <div className="card-body">
+                                        cata
+                                    </div>
+                                </div>
+                                </td></tr>
+                        )
+                        }
                 </div>
             </div>
 
